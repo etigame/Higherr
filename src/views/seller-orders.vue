345fdf81ed7e02c02d6aeb32b684487a02c908d9
Eti Gal-Or <etim139@gmail.com>
2022-11-29 22:52:29 +0200

    add routes

---
+++ src/views/seller-orders.vue
@@ -0,0 +1,11 @@
+<template>
+    <section class="seller-orders">
+        <h1>seller-orders</h1>
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'seller-orders'
+}
+</script>
---
+++ src/views/seller-register.vue
@@ -0,0 +1,11 @@
+<template>
+    <section class="seller-register">
+        <h1>seller-register</h1>
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'seller-register'
+}
+</script>
