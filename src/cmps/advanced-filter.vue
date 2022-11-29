ba4ff3863557bf782dba2451c6a853f2c73081a4
Eti Gal-Or <etim139@gmail.com>
2022-11-29 18:05:01 +0200

    add styles

---
+++ src/cmps/advanced-filter.vue
@@ -0,0 +1,11 @@
+<template>
+    <section class="advanced-filter">
+        <h1>advanced-filter</h1>
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'advanced-filter'
+}
+</script>
---
+++ src/views/gig-edit.vue
@@ -0,0 +1,11 @@
+<template>
+    <section class="gig-edit">
+        <h1>gig-edit</h1>
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'gig-edit',
+}
+</script>
