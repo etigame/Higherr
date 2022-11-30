4e4e3db4f439bbe1fb1ac52922733157f75c05e4
Roni Siles <rorokoo@gmail.com>
2022-11-30 22:14:20 +0200

    homepage sass

--- src/cmps/categories-list.vue
+++ src/cmps/categories-list.vue
@@ -1,37 +1,60 @@
 <template>
+
     <section class="categories-list main-layout full">
-        <section v-if="type==='cards'">
+        <section v-if="(type==='tag')" class="type-tag flex space-between" >
+            <h3 v-for="category in categories">{{category.name}}</h3>
+        </section>
+     
+    
+        <section v-else-if="(type==='card')" class="type-card ">
             <h1>Popular Professional Services</h1>
-            <div class="card" v-for="item in list">
-                <img :src="item.imageUrl"/>
+            <div class="cards flex space-between">
+            <div class="card" v-for="tag in tags" >
+                <h2>{{tag.subTxt}}  <h3>{{tag.txt}}</h3></h2>
+                <img :src="tag.imageUrl"/>
             </div>
+        </div>
         </section>
-        <section class="type-txt flex space-between" v-else-if="type==='txt'">
-            <h3 v-for="item in list">{{item.txt}}</h3>
+
+        <section v-else-if="(type==='svg')" class="type-svg" >
+            <span v-for="category in categories">
+                <span  v-icon="category.svg"></span>
+                <h4>{{category.name}}</h4>
+            </span>
         </section>
+
+
+       
     </section>
 </template>
 
 <script>
 
-import {subCategories} from "../services/categories-service.js"
+import categoriesService from "../services/categories-service.js"
 
 export default {
     name: 'categories-list',
+   
  props: {
         type: {
             type: String,
             required: true,
             
-        }
     },
+},
     data(){
-        return {
-          
-          list: subCategories
-
-
+        return{
+            categories: categoriesService.categories,
+            tags: categoriesService.tags
         }
+     
+    },
+    computed: {
+       getImage(item){
+        return {
+            backgroundImage: url(item.imageUrl)
+       } 
     }
+}
 }
 </script>
