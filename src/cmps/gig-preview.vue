c0b665162217dfcedaa92d5daf791a0836ed8584
Yair Orchen <yairorchen1@gmail.com>
2022-11-30 13:57:33 +0200

    editting the list preview and the preview card

--- src/cmps/gig-preview.vue
+++ src/cmps/gig-preview.vue
@@ -1,12 +1,24 @@
 <template>
     <section class="gig-preview">
-
+        <router-link :to="`/gig/${gig._id}`">
+            <div class="gig-img"><img :src="gig.image"></div>
+        </router-link>
+        <div class="seller-info-preview">
+        <div class="seller-img"><img :src="gig.imgUrl"></div>
+        <p>{{gig.fullname}}</p>
+        </div>
          <router-link :to="`/gig/${gig._id}`">
-            <h2> title: {{ gig.title }}</h2>
+            <h2>{{ gig.title }}</h2>
             </router-link>
-            <router-link :to="`/gig/${gig._id}`">
-            <div><img :src="gig.image"></div>
-            </router-link>
+            <div class="content-info">
+                
+            </div>
+        <div class="preview-footer">
+            <span v-icon="'fullHeart'" class="preview-heart"></span>
+            <p>starting at {{gig.price}}</p>
+        </div>
+
+            
 
                             <!-- <button @click="removeGig(gig._id)">x</button> -->
                             <!-- <button @click="updateGig(gig)">Update</button> -->
