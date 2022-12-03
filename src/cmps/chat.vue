6e66716b806d3a9f81808b3ad625305f4031b3d9
Yair Orchen <yairorchen1@gmail.com>
2022-12-03 23:06:19 +0200

    make the basic design of the chat

--- src/cmps/chat.vue
+++ src/cmps/chat.vue
@@ -1,10 +1,37 @@
 <template>
-  <div class="chat-seller">
-    <h2>{{ gig.fullname }}</h2>
-    <div class="img-seller">
-      <img :src="gig.imgUrl" alt="seller-img" />
+  <div v-if="!isOpen"  class="chat-seller-pill flex">
+      <div @click="toggleChat" class="seller-info flex">
+        <div class="img-seller">
+          <img :src="gig.imgUrl" alt="seller-img" />
+        </div>
+        <div class="info-txt flex">
+          <h4>Message {{ gig.fullname }}</h4>
+          <span>Avg. response time: {{ gig.avgResponseTime }}</span>
+        </div>
+      </div>
+  </div>
+
+
+  <div v-if="isOpen" class="chat-seller">
+    <div class="chat-seller-header flex">
+    <div class="seller-info flex">
+      <div class="img-seller">
+        <img :src="gig.imgUrl" alt="seller-img" />
+      </div>
+      <div class="info-txt flex">
+      <h2>Message {{ gig.fullname }}</h2>
+      <h4>Avg. response time: {{ gig.avgResponseTime }}</h4>
+      </div>
     </div>
-    <h4>{{ gig.avgResponseTime }}</h4>
+    <div @click="toggleChat"><span v-icon="'x'"></span></div>
+    </div>
+    <div class="text-explanation">
+    <span >Ask {{gig.fullname}} question or share your project details (requirements, timeline, budget, etc.)</span>
+    </div>
+    <div class="flex msg-area">
+      <div><span v-icon="'clip'"></span></div>
+      <div class="flex send-btn"><span v-icon="'send'"></span><p>Send Message</p></div>
+    </div>
   </div>
   
   
@@ -24,6 +51,7 @@ export default {
       // msg: { from: 'Guest', txt: '' },
       // msgs: [],
       // topic: 'Love',
+      isOpen:false
     }
   },
   created() {
@@ -36,7 +64,9 @@ export default {
     // socketService.terminate()
   },
   methods: {
-
+    toggleChat(){
+      this.isOpen=!this.isOpen
+    }
   }
 }
 </script>
