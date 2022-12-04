a03485a73f35bcfadc4feed6b150e6e1c04aa8de
Roni Siles <rorokoo@gmail.com>
2022-12-04 09:04:52 +0200

    homepage scss updates

--- src/views/app-home.vue
+++ src/views/app-home.vue
@@ -15,13 +15,12 @@
     <categories-list :type="'card'"  @filter="filter"/>
    
     <section class="video-section flex full main-layout">
-
       <div class="flex">
         <div class="site-info">
           <p class="side-text">
           <h1>A whole world of freelance talent at your fingertips</h1>
           <ul>
-            <li v-for="item in pageText">
+            <li v-for="item in getPageText">
               <span v-icon="'check'"></span>
               <h3>{{ item.title }}</h3>
               <p>{{ item.text }}</p>
@@ -30,10 +29,12 @@
           </p>
         </div>
         <div class="vid-container ">
-          <img src="src/assets/img/vid-teaser.webp">
+          <img  src="src/assets/img/vid-teaser.webp">
         </div>
       </div>
       </section>
+
+      
     <categories-list :type="'svg'"  @filter="filter"/>
   </section>
 </template>
@@ -48,15 +49,25 @@ export default {
  
   data() {
     return {
-      pageText: [
+      // pageText: [
+      //   { title: 'The best for every budget', text: ' Find high-quality services at every price point. No hourly rates, just project-based pricing.' },
+      //   { title: 'Quality work done quickly', text: ' Find the right freelancer to begin working on your project within minutes.' },
+      //   { title: 'Protected payments, every time', text: " Always know what you'll pay upfront. Your payment isn't released until you approve the work." },
+      //   { title: 'Quality work done quickly', text: ' Find the right freelancer to begin working on your project within minutes.' },
+      // ]
+
+    }
+  },
+  computed: {
+    getPageText(){
+      return [
         { title: 'The best for every budget', text: ' Find high-quality services at every price point. No hourly rates, just project-based pricing.' },
         { title: 'Quality work done quickly', text: ' Find the right freelancer to begin working on your project within minutes.' },
         { title: 'Protected payments, every time', text: " Always know what you'll pay upfront. Your payment isn't released until you approve the work." },
         { title: 'Quality work done quickly', text: ' Find the right freelancer to begin working on your project within minutes.' },
       ]
     }
-  },
-  computed: {
+
   },
   created() {
   },
