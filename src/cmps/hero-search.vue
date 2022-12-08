ae2d8b9d716bb5716d8d5779c83741621ae56fc2
Yair Orchen <yairorchen1@gmail.com>
2022-12-08 10:52:36 +0200

    search input hero working

--- src/cmps/hero-search.vue
+++ src/cmps/hero-search.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="hero-search flex">
+    <div class="flex input-container">
+      <div class="search-icon">
+      <span v-icon="'search'"></span>
+      </div>
     <input v-model="title" size="large" placeholder="Try 'Logo Design'">
+    </div>
     <!-- <el-input v-model="title" size="large" /> -->
     <el-button @click="filter"  size="large">Search</el-button>
   </div>
