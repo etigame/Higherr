1e9466d18836cb59c8fec7abddedeb40a14cf318
Roni Siles <rorokoo@gmail.com>
2022-12-08 15:57:20 +0200

    scss updates

--- src/cmps/hero-search.vue
+++ src/cmps/hero-search.vue
@@ -2,12 +2,12 @@
   <div class="hero-search flex">
     <div class="flex input-container">
       <div class="search-icon">
-      <span v-icon="'searchGrey'"></span>
+        <span v-icon="'searchGrey'"></span>
       </div>
-    <input v-model="title" size="large" placeholder="Try 'Logo Design'">
+      <input v-model="title" placeholder="Try 'Logo Design'">
     </div>
     <!-- <el-input v-model="title" size="large" /> -->
-    <el-button @click="filter"  size="large">Search</el-button>
+    <el-button @click="filter">Search</el-button>
   </div>
 </template>
   
