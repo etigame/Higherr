a0d2385ab45a6910e0058ebec3187a832db8b1e7
Yair Orchen <yairorchen1@gmail.com>
2022-12-13 11:00:26 +0200

    ui last fix and responsive

--- src/views/app-explore-list.vue
+++ src/views/app-explore-list.vue
@@ -62,7 +62,7 @@
               style="--el-switch-on-color: #1dbf73; --el-switch-off-color: #dadbdd" />
             <h4>Pro services</h4>
           </div>
-          <div class="online-switch"><el-switch v-model="demoInfo" class="ml-2"
+          <div class="online-switch"><el-switch v-model="demoInfo1" class="ml-2"
               style="--el-switch-on-color: #1dbf73; --el-switch-off-color: #dadbdd" />
             <h4>Online sellers</h4>
           </div>
@@ -118,6 +118,7 @@ export default {
       },
       budgetDrop: false,
       demoInfo: true,
+      demoInfo1: false,
       windowTop: window.top.scrollY,
       isShadow: false,
       deliveryOptions: [
